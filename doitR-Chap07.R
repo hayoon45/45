bc456a7e62d091ac1f96205817ac3a3e682f97da
hayoon45 <48703206+hayoon45@users.noreply.github.com>
2019-04-12 21:14:18 +0900

    Update doitR-Chap07.R

--- doitR-Chap07.R
+++ doitR-Chap07.R
@@ -1,4 +1,4 @@
-#혼자서 해보기 7-1
+#07-01 p.170
 library(ggplot2)
 library(dplyr)
 mpg <- as.data.frame(ggplot2::mpg)
@@ -12,23 +12,24 @@ mpg %>%
   group_by(drv) %>%
   summarise(mean_hwy = mean(hwy)) %>%
 
-#혼자서 해보기7-2
+#07-02 p.178
 library(ggplot2)
 library(dplyr)
 mpg <- as.data.frame(ggplot2::mpg)
 mpg[c(10, 14, 58, 93), "drv"] <- "k"
 mpg[c(29, 43, 129, 203), "cty"] <- c(3, 4, 39, 42)
-#Q1************
+#Q1
 table(mpg$drv)
+#%in% not use and use case.
 mpg$drv <- ifelse(mpg$drv == "k", NA, mpg$drv)
-table(is.na(mpg$drv))
+mpg$drv <- ifelse(mpg$drv %in% c("4", "f", "r"), mpg$drv, NA)
+table(mpg$drv)
 #Q2
-boxplot(mpg$cty)
 boxplot(mpg$cty)$stats
 mpg$cty <- ifelse(mpg$cty < 9 | mpg$cty > 26, NA, mpg$cty)
 boxplot(mpg$cty)
 #Q3
 mpg %>%
-  filter(!is.na(mpg$cty) & !is.na(mpg$drv)) %>%
+  filter(!is.na(cty) & !is.na(drv)) %>%
   group_by(drv) %>%
   summarise(mean_cty = mean(cty))
